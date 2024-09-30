54116095dc6f5419ea1a07f9780655916e4d1053
Saad Shaikh <shaikhsaad256@gmail.com>
2024-09-30 23:30:26 +0530

    Navbar Done

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Hotel Paradise - by shaikhsaad256@gmail.com</title>
+   <link rel="shortcut icon" href="Assets\Logo\Logo.svg" type="image/x-icon">
+
+   <!-- CSS File Link -->
+   <link rel="stylesheet" href="User Side\CSS\Home-Page.css">
+</head>
+
+<body>
+   <div class="header">
+      <div class="box"></div>
+      <div class="nav">
+         <div class="logo-links">
+            <img src="Assets\Logo\Logo.svg" alt="Logo">
+            <div class="links">
+               <a href="#">Home</a>
+               <a href="#">Rooms</a>
+               <a href="#">Things To Do</a>
+               <a href="#">Services</a>
+               <a href="#">Contact</a>
+            </div>
+         </div>
+         <div class="Social-CTA">
+            <div class="Social">
+               <a href="#"><img src="Assets\Icons\Twitter.svg" alt="Twitter"></a>
+               <a href="#"><img src="Assets\Icons\Facebook.svg" alt="Facebook"></a>
+               <a href="#"><img src="Assets\Icons\Instagram.svg" alt="Instagram"></a>
+            </div>
+            <div class="CTA">
+               <a href="">Logout</a>
+            </div>
+         </div>
+      </div>
+   </div>
+</body>
+
+</html>
